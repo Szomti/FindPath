0de36247ea66187b92333e66f3784c847a8a9359
Mateusz <mati.cibor1285@gmail.com>
2022-11-25 11:29:55 +0100

    No forceDarkAllowed; Setup improved layout; Setup lists with generated list;

--- app/src/main/java/com/example/findpath/MainActivity.kt
+++ app/src/main/java/com/example/findpath/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.findpath
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.findpath.databinding.ActivityMainBinding
+import com.example.findpath.databinding.ActivitySetupBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val setup = ActivitySetupBinding.inflate(layoutInflater)
+        val main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(setup.root)
+
+        val citiesList = generateDefaultList()
+
+        setupListsInit(setup, citiesList)
+
+        setup.setupFinishBtn.setOnClickListener {
+            setContentView(main.root)
+        }
+    }
+
+    private fun setupListsInit(setup: ActivitySetupBinding, list: MutableList<String>) {
+        val adapter = ArrayAdapter(this, R.layout.spinner_text, list)
+        setup.setupCityList.adapter = adapter
+        setup.setupCityListFirstItem.adapter = adapter
+        setup.setupCityListSecondItem.adapter = adapter
+        setup.setupCityListSecondItem.setSelection(1)
+    }
+
+    private fun generateDefaultList(): MutableList<String> {
+        val generatedList: MutableList<String> = mutableListOf()
+        for(i in 1..15){
+            generatedList.add("City$i")
+        }
+        return generatedList
     }
 }
